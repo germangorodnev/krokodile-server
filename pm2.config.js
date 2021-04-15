d3bfa7d16da37820f6c5a8a6a904e193ac41f16c
German Gorodnev <germangorodnev@gmail.com>
2021-04-15 12:42:52 +0700

    ss

--- pm2.config.js
+++ pm2.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'kroko-server',
-      script: 'npm run start:prod',
+      script: 'npm run start',
       instances: 1,
       autorestart: true,
       watch: false,
