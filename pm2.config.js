6d55ab0328dac65169001f61d611844da03ba699
German Gorodnev <germangorodnev@gmail.com>
2021-04-15 12:41:46 +0700

    ss

---
+++ pm2.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  apps: [
+    {
+      name: 'backend',
+      script: 'npm run start:prod',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '500M',
+      kill_timeout: 4000,
+      env: {
+        PRODUCTION: true,
+        NODE_ENV: 'production',
+        PORT: 6013,
+      },
+    }
+  ]
+}
